8f11aa42e5b9c7899631cd2b52d4befdf02f007b
Alex <bezproblemm@gmail.com>
2026-09-06 19:32:04 -0300

    05.09.2026
    1. Дополнил класс BusPatrolNanager логикой выбора лидера из автобусов, за котором будут по кругу следовать все остальные на определенном расстоянии.
    2. Убрал логику следования за лидером. Она приводила к столкновениям автобусов, при выезде с парковки на круг. Поменял логику в классах BusRoutePatrol и BusPatrolManager. Теперь при выезже с парковки на круг автобус будет пропускать тот автобус, который уже ездит по кругу и подъездает к 1 точке RoutePoint_1.

--- Assets/Scripts/Bus/BusPatrolManager.cs
+++ Assets/Scripts/Bus/BusPatrolManager.cs
@@ -3,20 +3,20 @@ using UnityEngine;
 
 public class BusPatrolManager : MonoBehaviour
 {
-   [SerializeField] private int _maxCurrentPatrols = 3;
-   
-   private readonly List<BusRoutePatrol> _activePatrols = new List<BusRoutePatrol>();
-   
-   public bool HasFreeSlot => _activePatrols.Count < _maxCurrentPatrols;
-
-   public void BusRegister(BusRoutePatrol busRoutePatrol)
-   {
-      if (!_activePatrols.Contains(busRoutePatrol))
-         _activePatrols.Add(busRoutePatrol);
-   }
-
-   public void BusUnregister(BusRoutePatrol busRoutePatrol)
-   {
-      _activePatrols.Remove(busRoutePatrol);
-   }
+    [SerializeField] private int _maxConcurrentPatrols = 3;
+ 
+    private readonly List<BusRoutePatrol> _activePatrols = new List<BusRoutePatrol>();
+ 
+    public bool HasFreeSlot => _activePatrols.Count < _maxConcurrentPatrols;
+ 
+    public void BusRegister(BusRoutePatrol patrol)
+    {
+        if (!_activePatrols.Contains(patrol))
+            _activePatrols.Add(patrol);
+    }
+ 
+    public void BusUnregister(BusRoutePatrol patrol)
+    {
+        _activePatrols.Remove(patrol);
+    }
 }
--- Assets/Scripts/Bus/BusRoutePatrol.cs
+++ Assets/Scripts/Bus/BusRoutePatrol.cs
@@ -4,98 +4,143 @@ using UnityEngine;
 public class BusRoutePatrol : MonoBehaviour
 {
     private const float MinMovementSqrMagnitude = 0.0001f;
-
-    [SerializeField] private float _moveSpeed = 7f;
+ 
+    [SerializeField] private float _moveSpeed = 4f;
     [SerializeField] private float _minSpacing = 2f;
     [SerializeField] private float _modelForwardOffsetY;
-
-    private readonly RoutePath _pathCalculator =  new RoutePath();
-
+ 
+    private readonly RoutePath _pathCalculator = new RoutePath();
+ 
+    private List<Vector3> _entryPath;
     private List<Vector3> _loopRoute;
+    private float _entryLength;
     private float _loopLength;
-    private BusRoutePatrol _leader;
     private BusPatrolManager _patrolManager;
     private bool _isPatrolling;
-
+    private bool _hasEnteredLoop;
+ 
     public float CurrentDistance { get; private set; }
     public bool IsPatrolling => _isPatrolling;
-    public BusRoutePatrol Leader => _leader;
-    
+    public bool HasEnteredLoop => _hasEnteredLoop;
+ 
+    public void StartPatrolling(BusRoute route, BusPatrolManager patrolManager)
+    {
+        var waypoints = new List<Vector3>(route.GetWaypointPositions());
+ 
+        _entryPath = BuildEntryPath(waypoints);
+        _entryLength = _pathCalculator.GetTotalLength(_entryPath);
+ 
+        _loopRoute = BuildLoopRoute(waypoints);
+        _loopLength = _pathCalculator.GetTotalLength(_loopRoute);
+ 
+        CurrentDistance = 0f;
+        _hasEnteredLoop = false;
+        _isPatrolling = waypoints.Count > 0;
+        _patrolManager = patrolManager;
+ 
+        _patrolManager.BusRegister(this);
+    }
+ 
+    public void StopPatrolling()
+    {
+        _isPatrolling = false;
+ 
+        _patrolManager?.BusUnregister(this);
+    }
+ 
+    private List<Vector3> BuildEntryPath(List<Vector3> waypoints)
+    {
+        if (waypoints.Count == 0)
+            return new List<Vector3> { transform.position };
+ 
+        return new List<Vector3> { transform.position, waypoints[0] };
+    }
+ 
+    private List<Vector3> BuildLoopRoute(List<Vector3> waypoints)
+    {
+        var loopRoute = new List<Vector3>(waypoints);
+ 
+        if (loopRoute.Count > 0)
+            loopRoute.Add(loopRoute[0]);
+ 
+        return loopRoute;
+    }
+ 
     private void Update()
     {
         if (!_isPatrolling)
             return;
-
-        float desireDistance = CalculateDesireDistance();
-
-        if (desireDistance <= CurrentDistance)
+ 
+        if (IsBlockedAhead())
             return;
-        
-        MoveTo(desireDistance);
+ 
+        float desiredDistance = CurrentDistance + _moveSpeed * Time.deltaTime;
+ 
+        MoveTo(desiredDistance);
     }
-
-    public void StartPatrolling(BusRoute route, BusPatrolManager patrolManager)
+ 
+    private bool IsBlockedAhead()
     {
-       _loopRoute = BuildLoopRoute(route);
-       _loopLength = _pathCalculator.GetTotalLength(_loopRoute);
-       CurrentDistance = 0f;
-        _isPatrolling = _loopRoute.Count > 1 && _loopLength > 0f;
-        _patrolManager = patrolManager;
-        
-        _patrolManager.BusRegister(this);
+        Collider[] hits = Physics.OverlapSphere(transform.position, _minSpacing);
+ 
+        foreach (Collider hit in hits)
+        {
+            if (!hit.TryGetComponent<BusRoutePatrol>(out var otherPatrol))
+                continue;
+ 
+            if (otherPatrol == this)
+                continue;
+ 
+            if (!otherPatrol.IsPatrolling)
+                continue;
+            
+            if (_hasEnteredLoop && !otherPatrol.HasEnteredLoop)
+                continue;
+ 
+            Vector3 toOther = otherPatrol.transform.position - transform.position;
+            toOther.y = 0f;
+ 
+            if (toOther.sqrMagnitude < MinMovementSqrMagnitude)
+                return true;
+ 
+            if (Vector3.Dot(toOther.normalized, transform.forward) > 0f)
+                return true;
+        }
+ 
+        return false;
     }
-
-    public void StopPatrolling()
-    {
-        _isPatrolling = false;
-        _patrolManager?.BusUnregister(this);
-    }
-
-    public void SetLeader(BusRoutePatrol leader)
-    {
-        _leader = leader;
-    }
-
-    private List<Vector3> BuildLoopRoute(BusRoute route)
-    {
-        var waypoints = new List<Vector3>(route.GetWaypointPositions());
-        
-        if (waypoints.Count > 0)
-            waypoints.Add(waypoints[0]);
-        
-        return waypoints;
-    }
-
-    private float CalculateDesireDistance()
-    {
-        float allowDistance = float.MaxValue;
-
-        if (_leader != null)
-            allowDistance = _leader.CurrentDistance - _minSpacing;
-
-        return Mathf.Min(CurrentDistance + _moveSpeed * Time.deltaTime, allowDistance);
-    }
-
+ 
     private void MoveTo(float distance)
     {
         CurrentDistance = distance;
-        float loopDistance = CurrentDistance % _loopLength;
-        
-        Vector3 newPosition = _pathCalculator.GetPointAtDistance(_loopRoute, loopDistance);
-        
-        RotateTowards(loopDistance);
-        
+ 
+        Vector3 newPosition;
+ 
+        if (CurrentDistance <= _entryLength)
+        {
+            newPosition = _pathCalculator.GetPointAtDistance(_entryPath, CurrentDistance);
+            RotateTowards(_entryPath, CurrentDistance);
+        }
+        else
+        {
+            _hasEnteredLoop = true;
+ 
+            float loopDistance = _loopLength > 0f ? (CurrentDistance - _entryLength) % _loopLength : 0f;
+            newPosition = _pathCalculator.GetPointAtDistance(_loopRoute, loopDistance);
+            RotateTowards(_loopRoute, loopDistance);
+        }
+ 
         transform.position = newPosition;
     }
-
-    private void RotateTowards(float loopDistance)
+ 
+    private void RotateTowards(List<Vector3> route, float distance)
     {
-        Vector3 direction = _pathCalculator.GetDirectionAtDistance(_loopRoute, loopDistance);
+        Vector3 direction = _pathCalculator.GetDirectionAtDistance(route, distance);
         direction.y = 0f;
-
+ 
         if (direction.sqrMagnitude < MinMovementSqrMagnitude)
             return;
-
+ 
         Quaternion lookRotation = Quaternion.LookRotation(direction, Vector3.up);
         transform.rotation = lookRotation * Quaternion.Euler(0f, _modelForwardOffsetY, 0f);
     }
